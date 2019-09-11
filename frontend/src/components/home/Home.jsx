33115d772f3498e9b430f46dd4e85af33af97e97
thallysbraz <thallysbraz3@gmail.com>
2019-09-11 10:23:10 -0300

    fazendo componente Home

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Main from "../template/Main";
+
+export default props => (
+  <Main icon="home" title="Início" subtitle="Crud react">
+    <div className="display-4">Bem-vindo!</div>
+    <hr />
+    <p className="mb-0">Exemplo de sistema de cadastro desenvolvido em React</p>
+  </Main>
+);
--- frontend/src/components/template/Main.jsx
+++ frontend/src/components/template/Main.jsx
@@ -5,6 +5,8 @@ import Header from "./Header";
 export default props => (
   <React.Fragment>
     <Header {...props} />
-    <main className="content">Conteudo</main>
+    <main className="content container-fluid">
+      <div className="p-3 mt-3">{props.children}</div>
+    </main>
   </React.Fragment>
 );
